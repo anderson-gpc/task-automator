7c25bcf33e6eac2fc0034e24599d14055e257c68
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-22 09:20:46 -0300

    feat: add GithubModule in app.module.ts

--- backend/app.module.ts
+++ backend/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { GithubModule } from "./src/modules/github.module";
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [ConfigModule.forRoot(), GithubModule],
 })
 
 export class AppModule {};
