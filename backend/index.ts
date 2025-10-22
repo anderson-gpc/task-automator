29ae7f4fe6078f578acd9da65cca98aef4de5f3b
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-22 09:18:17 -0300

    feat: create index.ts and app.module.ts

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+
+
+async function bootstrap() {
+  const configService = new ConfigService();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(configService.get<string>("NODE_PORT")!);
+}
+
+bootstrap();
