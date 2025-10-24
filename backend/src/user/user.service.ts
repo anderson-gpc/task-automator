dc978ed3fd61a72dbedfb636ba69497d3ff575a4
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-24 20:06:43 -0300

    refactor: add UserDTO in logic

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { User } from "../database/entity/user.entity";
 import type { ICrud } from "../utils/interfaces/crud.interface";
+import { UserDto } from "./dto/user.dto";
 
 @Injectable()
 export class UserService implements ICrud<User> {
@@ -11,12 +12,29 @@ export class UserService implements ICrud<User> {
     ) {}
 
     async create(data: Partial<User>) {
-        const existingUser = await this.verifyGithubUser(data.githubId!);
-        if (existingUser) {
-            return await this.userRepository.update(existingUser.id!, data);
-        };
-        const user = this.userRepository.create({...data})
-        return this.userRepository.save(user);
+        try {
+            const existingUser = await this.verifyGithubUser(data.githubId!);
+
+            if (existingUser)
+                await this.userRepository.update(existingUser.id!, data);
+            else {
+                const userEntity = this.userRepository.create({...data});
+                this.userRepository.save(userEntity);
+            }
+
+            const userDTO: UserDto = {
+                gitubId: existingUser!.githubId!,
+                username: existingUser!.username!,
+                photo: existingUser!.photo!,
+                displayName: existingUser!.displayName!,
+                acessToken: existingUser!.acessToken!
+            }
+
+            return userDTO;
+        } catch (error: any) {
+            throw new Error("Erro ao criar ou atualizar o acesso do usuário");
+        }
+
     }
 
     update(data: User | Partial<User>) {
