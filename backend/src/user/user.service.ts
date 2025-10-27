3d5efaec9564d589c36af2f0ffc50c2bccf77d08
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-27 10:43:33 -0300

    refactor: defining only 'user.service' for user actions.

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,57 +1,44 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { User } from "../database/entity/user.entity";
-import type { ICrud } from "../utils/interfaces/crud.interface";
+import type { ICreate } from "../utils/interfaces/crud.interface";
 import { UserDto } from "./dto/user.dto";
 
 @Injectable()
-export class UserService implements ICrud<User> {
-    constructor(
-        @Inject('USER_REPOSITORY')
-        private userRepository: Repository<User>
-    ) {}
+export class UserService implements ICreate<User> {
+  constructor(
+    @Inject("USER_REPOSITORY")
+    private userRepository: Repository<User>
+  ) {}
 
-    async create(data: Partial<User>) {
-        try {
-            const existingUser = await this.verifyGithubUser(data.githubId!);
+  async create(data: Partial<User>) {
+    try {
+      const existingUser = await this.verifyGithubUser(data.githubId!);
 
-            if (existingUser)
-                await this.userRepository.update(existingUser.id!, data);
-            else {
-                const userEntity = this.userRepository.create({...data});
-                this.userRepository.save(userEntity);
-            }
+      if (existingUser) {
+        await this.userRepository.update(existingUser.id!, data);
+      } else {
+        const userEntity = this.userRepository.create({ ...data });
+        this.userRepository.save(userEntity);
+      }
 
-            const userDTO: UserDto = {
-                gitubId: existingUser!.githubId!,
-                username: existingUser!.username!,
-                photo: existingUser!.photo!,
-                displayName: existingUser!.displayName!,
-                acessToken: existingUser!.acessToken!
-            }
+      const userDTO: UserDto = {
+        gitubId: existingUser!.githubId!,
+        username: existingUser!.username!,
+        photo: existingUser!.photo!,
+        displayName: existingUser!.displayName!,
+        acessToken: existingUser!.acessToken!,
+      };
 
-            return userDTO;
-        } catch (error: any) {
-            throw new Error("Erro ao criar ou atualizar o acesso do usuário");
-        }
-
+      return userDTO;
+    } catch (error: any) {
+      throw new Error("Erro ao criar ou atualizar o acesso do usuário");
     }
+  }
 
-    update(data: User | Partial<User>) {
-
-    }
-
-    delete(data: User | Partial<User>) {
-        
-    }
-
-    view(data: User | Partial<User>) {
-        
-    }
-
-    private async verifyGithubUser(githubId: string) {
-        return await this.userRepository.findOne({
-            where: {githubId: githubId}
-        })
-    }
+  private async verifyGithubUser(githubId: string) {
+    return await this.userRepository.findOne({
+      where: { githubId: githubId },
+    });
+  }
 }
