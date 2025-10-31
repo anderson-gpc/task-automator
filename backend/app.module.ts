82fbbc3f7a5a401f32f557a98bb4eaea4d34de04
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-31 16:37:06 -0300

    refactor: changing 'app.module.ts' for @Global

--- backend/app.module.ts
+++ backend/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from "@nestjs/config";
 import { GithubModule } from "./src/github/github.module";
 import { AuthModule } from "./src/auth/auth.module";
 import { RepositoryModule } from "./src/repository/repositorygithub.module";
+import { UserModule } from "./src/user/user.module";
 
 @Module({
-    imports: [ConfigModule.forRoot(), GithubModule, AuthModule, RepositoryModule],
+    imports: [ConfigModule.forRoot(), GithubModule, AuthModule, RepositoryModule, UserModule],
 })
 
 export class AppModule {};
