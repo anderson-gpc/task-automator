aa6721d782ef9231c6926dcd7d3323edd2884f54
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-05 08:35:07 -0300

    chore: implements relative paths in jest-tests

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,3 +1,6 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const {compilerOptions} = require("./tsconfig.json");
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
@@ -7,6 +10,9 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/../",
+  }),
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
 };
