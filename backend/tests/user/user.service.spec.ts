95eeda4ac570cf479e297b10ae607cef8d376869
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-05 14:28:20 -0300

    test: implements test for user.service

--- backend/tests/user/user.service.spec.ts
+++ backend/tests/user/user.service.spec.ts
@@ -4,25 +4,29 @@ import { EncryptionService } from "@/utils/encryption.service";
 import { ConfigService } from "@nestjs/config";
 import { Repository } from "typeorm";
 import { User } from "@/database/entity/user.entity";
+import { createMockRepository } from "@tests/utils/mock-repository";
 
 describe("UserService", () => {
   let service: UserService;
+  let encryptionService: EncryptionService;
   let userRepository: Repository<User>;
   let configService: ConfigService;
-  let encryptionService: EncryptionService;
+  let userRepoMock: ReturnType<typeof createMockRepository>;
 
   beforeEach(async () => {
+    userRepoMock = createMockRepository();
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
         EncryptionService,
         {
           provide: "USER_REPOSITORY",
-          useValue: {}, 
+          useValue: userRepoMock,
         },
         {
           provide: ConfigService,
-          useValue: {}, 
+          useValue: {},
         },
       ],
     }).compile();
@@ -35,5 +39,34 @@ describe("UserService", () => {
 
   it("deve estar definido", () => {
     expect(service).toBeDefined();
+  });
+
+  it("atualizar o 'acessToken' do usuário", async () => {
+    const existingUser: User = {
+      id: 1,
+      githubId: "12345",
+      displayName: "Fulano",
+      username: "fulano",
+      photo: "url",
+      acessToken: "presentAcessToken"
+    };
+
+    userRepoMock.findOne.mockResolvedValueOnce(existingUser);
+
+    const input = {
+      githubId: "12345",
+      displayName: "Fulano",
+      username: "fulano",
+      photo: "url",
+      acessToken: "newAcessToken",
+    } as User;
+
+    await service.createUser(input);
+
+    expect(userRepoMock.update).toHaveBeenCalledTimes(1);
+    expect(userRepoMock.update).toHaveBeenCalledWith(
+      { githubId: "12345" },
+      expect.objectContaining({ acessToken: "newAcessToken" })
+    );
   });
 });
