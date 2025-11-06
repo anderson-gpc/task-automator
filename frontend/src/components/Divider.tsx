95e394fe01acaff83c640de89473761f255b9f49
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-06 16:35:54 -0300

    feat: create divider component

---
+++ frontend/src/components/Divider.tsx
@@ -0,0 +1,25 @@
+import { ConfigProvider, Divider } from "antd";
+import { createStyles } from "antd-style";
+
+const userStyles = createStyles(({ prefixCls, css }) => ({
+  dividerStyle: css`
+    &.${prefixCls}-divider {
+        > span {
+            color: #fff;
+            font-weight: bold; 
+            font-size: 1.5rem;
+        }
+    }
+  `,
+}));
+
+export default function DividerComponent({ text }: { text: string }) {
+  const { styles } = userStyles();
+  return (
+    <ConfigProvider divider={{ className: styles.dividerStyle }}>
+      <Divider style={{ borderColor: "#fff" }} orientation="right">
+        {text}
+      </Divider>
+    </ConfigProvider>
+  );
+}
