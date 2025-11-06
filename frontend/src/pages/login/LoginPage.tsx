0e9d7e8e77a81e12ca88d50612f701ba46d625e1
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-06 19:04:41 -0300

    fix: moving the component's styling so that it is not read as a route file

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -3,22 +3,26 @@
 import { Flex } from "antd";
 import DividerComponent from "@/src/components/Divider";
 import ButtonComponent from "@/src/components/Button";
-import { loginStyles } from "./login.styles";
+import { loginStyles } from "@/src/assets/css/__login.styles";
 import auth from "@/src/app/actions/auth";
 
 export default function LoginPage() {
-
   async function handleClick() {
     const result = await auth();
-    if (result) console.log('ok');
-    else console.log('too bad');
+    if (result) console.log("ok");
+    else console.log("too bad");
   }
 
   return (
     <div style={loginStyles.containerStyle}>
       <div style={loginStyles.overlayStyle} />
 
-      <Flex vertical align="center" justify="center" style={loginStyles.boxStyle}>
+      <Flex
+        vertical
+        align="center"
+        justify="center"
+        style={loginStyles.boxStyle}
+      >
         <DividerComponent text="Login" />
         <ButtonComponent onClick={handleClick} />
       </Flex>
