0cf7ca4e87fa0ca9803965c164dff0ce88a11e45
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-07 10:08:03 -0300

    style: refactor styles files

--- frontend/src/assets/css/__login.styles.ts
+++ frontend/src/assets/css/__login.styles.ts
@@ -1,39 +1,43 @@
-const containerStyle: React.CSSProperties = {
-  width: "100vw",
-  height: "100vh",
+import { CSSProperties } from 'react';
+
+const containerStyle: CSSProperties = {
+  width: '100vw',
+  height: '100vh',
   backgroundImage: "url('/assets/bg.png')",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  backgroundRepeat: "no-repeat",
-  position: "relative",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 
-const overlayStyle: React.CSSProperties = {
-  position: "absolute",
+const overlayStyle: CSSProperties = {
+  position: 'absolute',
   inset: 0,
-  backgroundColor: "rgba(0, 0, 0, 0.7)",
+  backgroundColor: 'rgba(0, 0, 0, 0.7)',
   zIndex: 0,
 };
 
-const boxStyle: React.CSSProperties = {
+const boxStyle: CSSProperties = {
   width: 400,
-  padding: "40px 30px",
+  padding: '40px 30px',
   borderRadius: 16,
-  position: "relative",
-  overflow: "hidden",
-  backdropFilter: "blur(12px)",
-  background: "rgba(255, 255, 255, 0.15)",
-  boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-  color: "#fff",
-  textAlign: "center",
+  position: 'relative',
+  overflow: 'hidden',
+  backdropFilter: 'blur(12px)',
+  background: 'rgba(255, 255, 255, 0.15)',
+  boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+  color: '#fff',
+  textAlign: 'center',
   zIndex: 1,
 };
 
-export const loginStyles = {
-    containerStyle,
-    overlayStyle,
-    boxStyle
-}
+const useLoginStyles = () => ({
+  containerStyle,
+  overlayStyle,
+  boxStyle,
+});
+
+export default useLoginStyles;
--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -3,7 +3,7 @@
 import { Flex } from "antd";
 import DividerComponent from "@/components/Divider";
 import ButtonComponent from "@/components/Button";
-import { loginStyles } from "@/assets/css/__login.styles";
+import useLoginStyles from "@/src/assets/css/__login.styles";
 import login from "@/src/app/actions/login";
 
 export default function LoginPage() {
@@ -11,19 +11,20 @@ export default function LoginPage() {
     const url = await login();
     window.location.href = url;
   }
+  const { containerStyle, overlayStyle, boxStyle } = useLoginStyles();
 
   return (
-    <div style={loginStyles.containerStyle}>
-      <div style={loginStyles.overlayStyle} />
+    <div style={containerStyle}>
+      <div style={overlayStyle} />
 
       <Flex
         vertical
         align="center"
         justify="center"
-        style={loginStyles.boxStyle}
+        style={boxStyle}
       >
         <DividerComponent text="Login" />
-        <ButtonComponent onClick={handleClick} />
+        <ButtonComponent onClick={handleClick} text="Login com o Github"/>
       </Flex>
     </div>
   );
