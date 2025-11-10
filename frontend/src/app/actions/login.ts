de600c14875316f7c7edb5f8b2b0d5846372f576
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-10 15:47:07 -0300

    refactor: implements UserDTO

--- frontend/src/app/actions/login.ts
+++ frontend/src/app/actions/login.ts
@@ -3,16 +3,26 @@
 import { githubClient } from "@/src/github.client";
 import { cookies } from "next/headers";
 import * as jose from "jose";
+import { UserDTO } from "@/src/dto/UserDTO";
 
 const SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
 
 export default async function login(): Promise<any> {
-  const { data } = await githubClient.request("GET /user");
-  const jwt = await new jose.SignJWT(data)
-    .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setExpirationTime("15min")
-    .sign(SECRET);
-  (await cookies()).set("access_token", jwt);
-  return data;
+  try {
+    const { data } = await githubClient.request("GET /user");
+    const jwt = await new jose.SignJWT(data)
+      .setProtectedHeader({ alg: "HS256" })
+      .setIssuedAt()
+      .setExpirationTime("15min")
+      .sign(SECRET);
+    (await cookies()).set("access_token", jwt, { httpOnly: true, path: "/" });
+    return new UserDTO({
+      login: data.login,
+      avatar_url: data.avatar_url,
+      url: data.html_url,
+      name: data.name!,
+    });
+  } catch (error) {
+    throw new Error("Erro ao fazer login");
+  }
 }
