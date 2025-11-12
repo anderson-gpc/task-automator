0ba69cecf6e949c739f1c0aa42e5125b4e5d5e98
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-12 11:58:52 -0300

    feat: creating a component of the ThreeDot

---
+++ frontend/src/components/ThreeDot.tsx
@@ -0,0 +1,12 @@
+import { ThreeDot } from "react-loading-indicators";
+
+export default function ThereeDotComponent({ text }: { text: string }) {
+  return (
+    <ThreeDot
+      variant="bounce"
+      color="rgba(47, 40, 144, 0.85)"
+      text={text}
+      size="large"
+    />
+  );
+}
