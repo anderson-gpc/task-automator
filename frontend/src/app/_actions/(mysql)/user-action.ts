0ac89d56e5107ce2bfc68fe776c1f6f1d6ad9bd4
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-13 15:19:12 -0300

    feat: creating server actions for the user

---
+++ frontend/src/app/_actions/(mysql)/user-action.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import {prisma} from "@/lib/(prisma)/prisma";
+import { User } from "@/src/interfaces/user-interface";
+import { PrismaError } from "@/src/lib/(prisma)/PrismaError";
+
+export async function createUser(data: User): Promise<boolean> {
+  try {
+    console.log('Try');
+    await prisma.user.create({
+      data: data,
+    });
+    return true;
+  } catch (e) {
+    throw PrismaError.handle(e);
+  }
+}
+
+export async function verifyUser(data: User): Promise<boolean> {
+  try {
+    const user = await prisma.user.findFirst({ where: { githubId: data.githubId } });
+    if (user != null) return true;
+    else {
+      await createUser(data)
+      return true;
+    }
+  } catch (e) {
+    throw PrismaError.handle(e);
+  }
+}
