823d38262cde99b472320f1e4bdc7d564ff7cc18
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-13 15:28:51 -0300

    style: apply motion.div in Login

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -6,6 +6,7 @@ import ButtonComponent, { ButtonStyleType } from "@/components/Button";
 import useLoginStyles from "@/src/assets/css/__login.styles";
 import { GithubOutlined } from "@ant-design/icons";
 import githubLogin from "@/src/app/_actions/(github)/login-action";
+import { motion } from "framer-motion";
 
 export default function LoginPage() {
   const { containerStyle, overlayStyle, boxStyle } = useLoginStyles();
@@ -13,21 +14,26 @@ export default function LoginPage() {
   return (
     <div style={containerStyle}>
       <div style={overlayStyle} />
-
-      <Flex vertical align="center" justify="center" style={boxStyle}>
-        <DividerComponent text="Login" />
-        <Form onFinish={githubLogin}>
-          <Form.Item>
-            <ButtonComponent
-              htmlType="submit"
-              stylesButton={ButtonStyleType.Gradient}
-              icon={<GithubOutlined />}
-              onClick={() => {}}
-              text="Login com o Github"
-            />
-          </Form.Item>
-        </Form>
-      </Flex>
+      <motion.div
+        initial={{ y: 0, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
+        <Flex vertical align="center" justify="center" style={boxStyle}>
+          <DividerComponent text="Login" />
+          <Form onFinish={githubLogin}>
+            <Form.Item>
+              <ButtonComponent
+                htmlType="submit"
+                stylesButton={ButtonStyleType.Gradient}
+                icon={<GithubOutlined />}
+                onClick={() => {}}
+                text="Login com o Github"
+              />
+            </Form.Item>
+          </Form>
+        </Flex>
+      </motion.div>
     </div>
   );
 }
