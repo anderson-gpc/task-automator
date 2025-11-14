e3aadae29a539610ddf8be47988746ace0ef0f92
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-14 08:21:20 -0300

    feat: insert into the modal component on the DefaultPage

--- frontend/src/pages/home/DefaultPage.tsx
+++ frontend/src/pages/home/DefaultPage.tsx
@@ -1,9 +1,21 @@
+"use client"
+
+import { InfoCircleFilled } from "@ant-design/icons";
 import { Slab } from "react-loading-indicators";
+import ModalComponent from "@/components/Modal";
+import { InfoContent } from "@/src/constants/texts";
 
 export default function DefaultPage() {
   return (
-    <div className="flex w-full h-full justify-center items-center">
-      <Slab size="large" color="rgba(47, 40, 144, 0.85)" text="Insira e salve seu token de acesso pessoal do GitHub para habilitar as funcionalidades." />
+    <div className="flex flex-col w-full h-full justify-center items-center">
+      <Slab
+        size="large"
+        color="rgba(47, 40, 144, 0.85)"
+        text="Insira e salve seu token de acesso pessoal do GitHub para habilitar as funcionalidades."
+      />
+      <ModalComponent footer={true} icon={<InfoCircleFilled />} text="Informações" title="Token de acesso refinado" content={[
+        InfoContent
+      ]} />
     </div>
   );
 }
