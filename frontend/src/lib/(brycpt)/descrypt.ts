e1b05e81b3994c9fc10d9454c667a1a300f109ee
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-15 14:48:20 -0300

    feat: creating a function to perfom token encryption

---
+++ frontend/src/lib/(brycpt)/descrypt.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import crypto from "crypto";
+
+const key = Buffer.from(process.env.CRYPT_KEY!, "base64");
+
+export async function descrypt(token: string) {
+  const [encryptedBase64, ivBase64, tagBase64] = token.split(":");
+
+  const encrypted = Buffer.from(encryptedBase64, "base64");
+  const iv = Buffer.from(ivBase64, "base64");
+  const tag = Buffer.from(tagBase64, "base64");
+
+  const deciper = crypto.createDecipheriv("aes-256-gcm", key, iv);
+  deciper.setAuthTag(tag);
+
+  const descrypted = Buffer.concat([
+    deciper.update(encrypted),
+    deciper.final(),
+  ]);
+}
---
+++ frontend/src/lib/(brycpt)/encrypt.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import crypto from "crypto";
+import { descrypt } from "./descrypt";
+
+
+const key = Buffer.from(process.env.CRYPT_KEY!, "base64");
+
+export default async function encrypt(token: string): Promise<void> {
+  const iv = crypto.randomBytes(12);
+  const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
+
+  const encrypted = Buffer.concat([cipher.update(token), cipher.final()]);
+  const tag = cipher.getAuthTag();
+
+  const tokenEncrypted = [
+    encrypted.toString("base64"),
+    iv.toString("base64"),
+    tag.toString("base64"),
+  ].join(":");
+
+  console.log(tokenEncrypted);
+  await descrypt(tokenEncrypted);
+}
