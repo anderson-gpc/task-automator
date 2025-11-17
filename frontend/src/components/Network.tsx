18cc8aecd7ac7a3b3dce8eb328e5668a93f72e24
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-17 15:03:38 -0300

    style: show feedback for user

--- frontend/src/components/Network.tsx
+++ frontend/src/components/Network.tsx
@@ -3,6 +3,8 @@
 import { Avatar, Button, Card, Flex, Typography } from "antd";
 import { LinkOutlined, UserDeleteOutlined } from "@ant-design/icons";
 import { NetworkInterface } from "@/src/interfaces/network-interface";
+import { Colors } from "../constants/colors";
+import { useState } from "react";
 
 interface NetworkProps {
   data: NetworkInterface;
@@ -15,29 +17,46 @@ export default function NetworkComponent({
   onRemove,
   onView,
 }: NetworkProps) {
+  const [isRemoved, setRemoved] = useState(false);
+
+  const handleRemove = () => {
+    if (isRemoved) return; // evita cliques repetidos
+    onRemove?.(data);
+    setRemoved(true);
+  };
+
+  const handleView = () => onView?.(data);
+
   return (
-    <Card style={{ margin: "1rem" }}>
+    <Card
+      style={{
+        margin: "1rem",
+        background: isRemoved ? Colors.Gray015 : Colors.White,
+        transition: "background 0.2s ease",
+      }}
+    >
       <Flex vertical gap="0.75rem">
         <Flex align="center" gap="1rem">
           <Avatar src={data.avatar_url} />
           <Typography.Text strong>{data.login}</Typography.Text>
         </Flex>
 
-        <Flex gap="0.5rem" justify="space-between" wrap={true}>
+        <Flex gap="0.5rem" justify="space-between" wrap>
           <Button
             icon={<UserDeleteOutlined />}
             danger
             type="primary"
-            onClick={() => onRemove?.(data)}
+            onClick={handleRemove}
+            disabled={isRemoved}
             style={{ flex: 1, minWidth: "12rem", maxWidth: "15rem" }}
           >
-            Remover seguidor
+            {isRemoved ? "Removido" : "Remover"}
           </Button>
 
           <Button
             icon={<LinkOutlined />}
             type="primary"
-            onClick={() => onView?.(data)}
+            onClick={handleView}
             style={{ flex: 1, minWidth: "12rem", maxWidth: "15rem" }}
           >
             Visualizar perfil
